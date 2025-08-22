ee0b7c85ffd840b109ee5c49e1251e3641628023
KakaSantosSilva <santoskauan758@gmail.com>
2025-08-22 17:15:26 -0300

    Inicio dos estudos com Função

---
+++ ParOuImpar.js
@@ -0,0 +1,13 @@
+//Par ou impar
+parOuimpar(2) //
+
+parOuimpar(5)
+
+function parOuimpar(n){
+    
+    if(n % 2 === 0){
+        console.log(n + " é um número par")
+    }else{
+        console.log(n + " é um número ímpar")
+    }
+}
